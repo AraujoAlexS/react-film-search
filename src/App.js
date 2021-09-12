2a61ffcb3554f25e6cd04dab243f0d463a7bfea5
AraujoAlexS <araujo.alexsantos@gmail.com>
2021-09-12 17:45:26 -0300

    Header add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
-
+import React from "react";
+import { GlobalStyle } from "./GlobalStyle";
+//Components
+import Header from "./components/Header";
 function App() {
   return (
     <div className="App">
-      Start here.
+      <Header />
+      <GlobalStyle />
     </div>
   );
 }
