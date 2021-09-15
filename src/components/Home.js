e718c4993adede298c002c9eda4c5ff13fbe6187
AraujoAlexS <araujo.alexsantos@gmail.com>
2021-09-15 14:30:07 -0300

    Spinner added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 //API
 import API from "../API";
 // config
@@ -7,6 +7,7 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from "../config";
 import HeroImage from "./HeroImage";
 import Grid from "./Grid";
 import Thumb from "./Thumb";
+import Spinner from "./Spinner";
 // HOOK
 import { useHomeFetch } from "../hooks/useHomeFetch";
 // IMG
@@ -39,6 +40,7 @@ const Home = () => {
           />
         ))}
       </Grid>
+      <Spinner></Spinner>
     </div>
   );
 };
