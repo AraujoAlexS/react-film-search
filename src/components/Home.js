8929f81495e5901354c35a8ab4ad9dff708d6c5e
AraujoAlexS <araujo.alexsantos@gmail.com>
2021-09-16 10:57:41 -0300

    Search Bar addedd

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,25 +8,27 @@ import HeroImage from "./HeroImage";
 import Grid from "./Grid";
 import Thumb from "./Thumb";
 import Spinner from "./Spinner";
+import SearchBar from "./SearchBar";
 // HOOK
 import { useHomeFetch } from "../hooks/useHomeFetch";
 // IMG
 import NoImage from "../images/no_image.jpg";
 
 const Home = () => {
-  const { state, loading, error } = useHomeFetch();
+  const { state, loading, error, searchTerm, setSearchTerm } = useHomeFetch();
   console.log(state);
 
   return (
     <div>
-      {state.results[0] ? (
+      {!searchTerm && state.results[0] ? (
         <HeroImage
           image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
           title={state.results[0].original_title}
           text={state.results[0].overview}
         />
       ) : null}
-      <Grid header="Popular Movies">
+      <SearchBar setSearchTerm={setSearchTerm} />
+      <Grid header={searchTerm ? "Search Result" : "Popular Movies"}>
         {state.results.map((movie) => (
           <Thumb
             key={movie.id}
@@ -40,7 +42,7 @@ const Home = () => {
           />
         ))}
       </Grid>
-      <Spinner></Spinner>
+      <Spinner />
     </div>
   );
 };
--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -9,6 +9,7 @@ const initialState = {
 };
 
 export const useHomeFetch = () => {
+  const [searchTerm, setSearchTerm] = useState("");
   const [state, setState] = useState(initialState);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
@@ -31,10 +32,11 @@ export const useHomeFetch = () => {
     setLoading(false);
   };
 
-  // INITIAL RENDER
+  // INITIAL RENDER AND SEARCH
   useEffect(() => {
-    fetchMovies(1);
-  }, []);
+    setState(initialState);
+    fetchMovies(1, searchTerm);
+  }, [searchTerm]);
 
-  return { state, loading, error };
+  return { state, loading, error, searchTerm, setSearchTerm };
 };
