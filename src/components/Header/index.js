4329bf6b0fe2f3fa34af6f8f41503c5109fe661b
AraujoAlexS <araujo.alexsantos@gmail.com>
2021-09-17 17:04:16 -0300

    Fetch movie and routes added

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,12 +2,17 @@ import React from "react";
 import RMDBLogo from "../../images/react-movie-logo.svg";
 import TMDBLogo from "../../images/tmdb_logo.svg";
 import { Wrapper, Content, LogoImg, TMDBLogoImg } from "./Header.styles";
+import { Link } from "react-router-dom";
 
 const Header = () => (
   <Wrapper>
     <Content>
-      <LogoImg src={RMDBLogo} alt="rmdb-logo" />
-      <TMDBLogoImg src={TMDBLogo} alt="tmdb-logo" />
+      <Link to="/">
+        <LogoImg src={RMDBLogo} alt="rmdb-logo" />
+      </Link>
+      <a href="https://www.themoviedb.org/?language=pt-BR">
+        <TMDBLogoImg src={TMDBLogo} alt="tmdb-logo" />
+      </a>
     </Content>
   </Wrapper>
 );
